56599709c0624bd8ab7a30ef4c9ecd4409171641
unknown <sakshiraorane.2005@gmail.com>
2026-06-06 20:00:15 +0530

    Initial portfolio

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,7 @@
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
 export default defineConfig({
-  vite: {
-    base: "/PRODIGY_WD_04/",
-  },
+
 
   tanstackStart: {
     server: {
